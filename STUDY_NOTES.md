65cf0a4e936f8d693ff0a1d02a1ab2d1e4e23bd7
gzhennaxia <gzhennaxia@163.com>
2019-05-26 00:26:49 +0800

    home swiper first commit

--- STUDY_NOTES.md
+++ STUDY_NOTES.md
@@ -42,6 +42,34 @@ UI 给的是二倍图，所以能得到如下关系：
 
 技巧：行高等于元素高，可将文本内容垂直居中
 
+### 防止页面抖动
+
+当用户网速过慢时，如果图片还没加载完，图片下面的元素会先渲染，图片加载完后会再次渲染，导致页面出现抖动。通过以下 css 来防止页面抖动：
+
+```html
+<div class="wrapper">
+  <swiper :options="swiperOption">
+    <swiper-slide v-for="item of imgList" :key="item.id">
+      <img class="swiper-img" :src="item.value"/>
+    </swiper-slide>
+    <div class="swiper-pagination" slot="pagination"></div>
+  </swiper>
+</div>
+```
+
+```css
+.wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 26.6% /* 图片宽高比 */
+    background: #eee
+    .swiper-img
+      width: 100%
+```
+
+
+
 ## Stylus
 ### 变量
 新建后缀名为 `.styl` 的文件，在 styl 文件中定义变量。例如：`$bgColor = #00bcd4`
@@ -75,3 +103,7 @@ resolve: {
 > 注意：在 css 中使用路径别名需要在前面加上 `~` 
 >
 > [Webpack 中 css import 使用 alias 相对路径](https://wiki.zthxxx.me/wiki/%E6%8A%80%E6%9C%AF%E5%BC%80%E5%8F%91/%E5%89%8D%E7%AB%AF/Webpack-%E4%B8%AD-css-import-%E4%BD%BF%E7%94%A8-alias-%E7%9B%B8%E5%AF%B9%E8%B7%AF%E5%BE%84/)
+
+## 三方组件
+### vue-awesome-swiper
+> [GitHub 地址](https://github.com/surmon-china/vue-awesome-swiper)
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl"/>
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination'
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/b23a39921e8b78f38b61412d691d93ea.jpg_750x200_942ed7bd.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/87a224d0349d94a11e97f31aa1aba4f5.jpg_750x200_1f78af87.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 26.6%
+    background: #eee
+    .swiper-img
+      width: 100%
+</style>
