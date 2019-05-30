f2afc4303d57bcbc542f893a1c31b622e85deaca
gzhennaxia <gzhennaxia@163.com>
2019-05-31 06:59:42 +0800

    develpment over

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="icons">
+    <swiper>
+      <swiper-slide v-for="(page, index) of pages" :key="index">
+        <div class="icon" v-for="icon of page" :key="icon.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="icon.imgUrl">
+          </div>
+          <p class="icon-desc">{{icon.desc}}</p>
+        </div>
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data(){
+    return {
+      iconList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票'
+      }, {
+        id: '0002',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20195/b0026eb6f96fd2daecba0d7982c7e4c8.png',
+        desc: '5月特惠'
+      },]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/variables.styl"
+  @import "~styles/mixins.styl"
+  .icons >>> .swiper-container
+    height: 0
+    padding-bottom: 50%
+  .icon
+    position: relative
+    overflow: hidden
+    float: left
+    width: 25%
+    height: 0
+    padding-bottom: 25%
+    .icon-img
+      position: absolute
+      top: 0
+      left: 0
+      right: 0
+      bottom: .44rem
+      .icon-img-content
+        height: 100%
+        display: block
+        margin: 0 auto
+    .icon-desc
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      height: .44rem
+      line-height: .44rem
+      text-align: center
+      color: $darkTextColor
+      ellipsis()
+</style>
